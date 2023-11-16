db111486cb2e66446d926a4f5b687b47cf21fbc0
Vibhinn Singhal <vibhinnsinghal_ec21a18_61@dtu.ac.in>
2023-11-16 21:34:30 +0530

    basic blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,50 +1,51 @@
 import hashlib
-import time
 
-class Block:
-    def __init__(self, index, transactions, timestamp, previous_hash):
-        self.index = index
-        self.transactions = transactions
+class User:
+    def __init__(self, username, public_key):
+        self.username = username
+        self.public_key = public_key
+        self.posts = []
+
+class Post:
+    def __init__(self, author, content, timestamp):
+        self.author = author
+        self.content = content
         self.timestamp = timestamp
-        self.previous_hash = previous_hash
         self.hash = self.calculate_hash()
 
     def calculate_hash(self):
         sha = hashlib.sha256()
-        sha.update(str(self.index).encode('utf-8'))
-        sha.update(str(self.transactions).encode('utf-8'))
+        sha.update(str(self.author).encode('utf-8'))
+        sha.update(self.content.encode('utf-8'))
         sha.update(str(self.timestamp).encode('utf-8'))
-        sha.update(str(self.previous_hash).encode('utf-8'))
         return sha.hexdigest()
 
 class Blockchain:
     def __init__(self):
         self.chain = []
+        self.users = {}
 
     def create_genesis_block(self):
-        genesis_block = Block(0, [], 0, '0')
+        genesis_block = Post(None, 'Genesis block', 0)
         self.chain.append(genesis_block)
 
-    def add_block(self, block):
-        if block.previous_hash != self.chain[-1].hash:
+    def add_block(self, post):
+        if post.calculate_hash() != self.chain[-1].hash:
             raise Exception('Invalid previous hash')
 
-        block.hash = block.calculate_hash()
-        self.chain.append(block)
+        self.chain.append(post)
 
     def get_last_block(self):
         return self.chain[-1]
 
-blockchain = Blockchain()
-blockchain.create_genesis_block()
+    def add_user(self, username, public_key):
+        new_user = User(username, public_key)
+        self.users[username] = new_user
 
-transaction = {
-    'sender': 'Alice',
-    'recipient': 'Bob',
-    'amount': 10
-}
+    def create_post(self, author, content, timestamp):
+        if author not in self.users:
+            raise Exception(f'User {author} not found')
 
-block = Block(1, [transaction], time.time(), blockchain.get_last_block().hash)
-blockchain.add_block(block)
-
-print(blockchain.chain)
+        new_post = Post(author, content, timestamp)
+        self.users[author].posts.append(new_post)
+        self.add_block(new_post)
